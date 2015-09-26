78dfeeb979aa5b7f37c85c6272a0f2d9cb57ad4c
ostyn <ostyn@localhost>
2015-09-26 17:42:34 -0600

    Added some end game conditions and you can no longer play twice on the same square.

--- tictactoe.js
+++ tictactoe.js
@@ -1,55 +1,74 @@
 export class TicTacToe {
 	constructor() {
 		this.size = 3;
+		this.newGame();
+		this.movesLeft = this.size*this.size;
+	}
+	newGame() {
+		this.message = "";
 		this.activeToken = 'O'
 		this.grid = [];
-		this.gameOver = false;
-		for(var column = 0; column < this.size; column++) {
-			for(var row = 0; row < this.size; row++) {
-				if(!this.grid[row])
+		this.winningPlayer = "";
+		for (var column = 0; column < this.size; column++) {
+			for (var row = 0; row < this.size; row++) {
+				if (!this.grid[row])
 					this.grid[row] = [];
 				this.grid[row][column] = "";
 			}
 		}
 	}
 	play(token, x, y) {
-		this.grid[x][y] = token;
-		this.gameOver = this.hasGameEnded();
+		this.message = "";
+		if (this.winningPlayer !== "" || this.movesLeft === 0) {
+			this.message = "click new game to start again";
+			return;
+		}
+		if (this.grid[x][y] === "") {
+			this.grid[x][y] = token;
+			this.winningPlayer = this.hasGameEnded();
+			this.movesLeft--;
+			if (this.winningPlayer !== "" || this.movesLeft === 0)
+				this.message = "click new game to start again";
+		} else {
+			this.message = "Bad move. Choose an empty space";
+			this.activePlayer();
+		}
 	}
 	activePlayer() {
-		if(this.activeToken === 'X')
+		var active = this.activeToken;
+		if (this.activeToken === 'X')
 			this.activeToken = 'O'
 		else
 			this.activeToken = 'X'
-		return this.activeToken;
+		return active;
 	}
 	hasGameEnded() {
-		if(this.checkLine(0, 0, [0,1], this.size))
-			return true;
-		else if(this.checkLine(1, 0, [0,1], this.size))
-			return true;
-		else if(this.checkLine(2, 0, [0,1], this.size))
-			return true;
-		else if(this.checkLine(0, 0, [1,0], this.size))
-			return true;
-		else if(this.checkLine(0, 1, [1,0], this.size))
-			return true;
-		else if(this.checkLine(0, 2, [1,0], this.size))
-			return true;
-		else if(this.checkLine(0, 0, [1,1], this.size))
-			return true;
-		else if(this.checkLine(0, 2, [1,-1], this.size))
-			return true;
-		return false;
+		if (this.checkLine(0, 0, [0, 1], this.size))
+			return this.grid[0][0];
+		else if (this.checkLine(1, 0, [0, 1], this.size))
+			return this.grid[1][0];
+		else if (this.checkLine(2, 0, [0, 1], this.size))
+			return this.grid[2][0];
+		else if (this.checkLine(0, 0, [1, 0], this.size))
+			return this.grid[0][0];
+		else if (this.checkLine(0, 1, [1, 0], this.size))
+			return this.grid[0][1];
+		else if (this.checkLine(0, 2, [1, 0], this.size))
+			return this.grid[0][2];
+		else if (this.checkLine(0, 0, [1, 1], this.size))
+			return this.grid[0][0];
+		else if (this.checkLine(0, 2, [1, -1], this.size))
+			return this.grid[0][2];
+		return "";
 	}
-	checkLine(x, y, vector, size){
+	checkLine(x, y, vector, size) {
 		var startingValue = this.grid[x][y];
-		if(startingValue === "")
+		if (startingValue === "")
 			return false;
-		for(var i = 0; i < size-1; i++) {
+		for (var i = 0; i < size - 1; i++) {
 			x = x + vector[0];
 			y = y + vector[1];
-			if(startingValue !== this.grid[x][y])
+			if (startingValue !== this.grid[x][y])
 				return false;
 		}
 		return true;
