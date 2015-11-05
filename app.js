22c59d45cde339e448b99833ab9dd48ca4d87809
ostyn <ostyn@localhost>
2015-11-04 22:19:40 -0700

    This should have been in the last commit.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ export class App {
 		config.map([
 		  { route: ['','home'], name: 'home', moduleId: 'Home', nav: true, title:'Home' },
 		  { route: ['cards/:id'], name: 'cards', moduleId: 'Cards', nav: false, title:'cards' },
-		  { route: ['ttt'], name: 'ttt', moduleId: 'TicTacToe', nav: true, title:'Tic-Tac-Toe' },
+		  { route: ['ultimateTTT'], name: 'ultimateTicTacToe', moduleId: 'UltimateTicTacToe', nav: true, title:'Ultimate Tic-Tac-Toe' },
 		]);
 
 		this.router = router;
