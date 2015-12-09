195710024b3255623b2e6662ede0336b7a04f562
ostyn <ostyn@localhost>
2015-12-09 00:17:33 -0700

    Finishing the rename from GameLogic to TicTacToeLogic

--- tic-tac-toe/TicTacToeLogic.js
+++ tic-tac-toe/TicTacToeLogic.js
@@ -1,23 +1,23 @@
-export class GameLogic {
+export class TicTacToeLogic {
 	static hasGameEnded(size, grid) {
 		//Columns
 		for(var i = 0; i < size; i++) {
-			var check = GameLogic.checkLine(i, 0, [0, 1], size, grid);
+			var check = TicTacToeLogic.checkLine(i, 0, [0, 1], size, grid);
 			if (check !== "-") {
 				return check;
 			}
 		}
 		//Rows
 		for(var i = 0; i < size; i++) {
-			var check = GameLogic.checkLine(0, i, [1, 0], size, grid);
+			var check = TicTacToeLogic.checkLine(0, i, [1, 0], size, grid);
 			if (check !== "-") {
 				return check;
 			}
 		}
 		//Diagonals
 		if (size % 2 === 1) {
-			var diagonal1 = GameLogic.checkLine(0, 0, [1, 1], size, grid);
-			var diagonal2 = GameLogic.checkLine(0, size-1, [1, -1], size, grid);
+			var diagonal1 = TicTacToeLogic.checkLine(0, 0, [1, 1], size, grid);
+			var diagonal2 = TicTacToeLogic.checkLine(0, size-1, [1, -1], size, grid);
 			if (diagonal1 !== "-") {
 				return diagonal1;
 			}
